3d436f0f5aa57168f95740dc6fa0c94042212020
changchang17 <yischang.li@posteo.de>
2021-07-28 16:25:44 +0100

    Add Test for Java

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -3,7 +3,9 @@ class TagManipulator:
     def parse_string(self, tags):
         result = []
 
-        if len(tags) < 1:
+        if len(tags) < 1 or tags == ",":
             return result
+
+        result = tags.split(",")
 
         return result
--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -27,3 +27,31 @@ def test_split_comma_empty_string_result_empty_array():
 
     # assert
     assert result == expResult
+
+
+def test_java_result_java():
+    # arrange
+    stringToSplit = "java"
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+
+def test_java_python_result_java_python():
+    # arrange
+    stringToSplit = "java"
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
